6b06d6b990f7585ff796d16bc8c3377494d9be44
Ben Surigao <bsurigao@gmail.com>
2020-03-19 19:59:06 -0700

    adds css, app.js, config.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,114 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Chatkit from '@pusher/chatkit-client'
+import MessageList from './components/MessageList'
+import SendMessageForm from './components/SendMessageForm'
+import RoomList from './components/RoomList'
+import NewRoomForm from './components/NewRoomForm'
 
-function App() {
+import { tokenUrl, instanceLocator } from './config'
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      roomId: null,
+      messages: [],
+      joinableRooms: [],
+      joinedRooms: []
+    }
+    this.sendMessage = this.sendMessage.bind(this)
+    this.subscribeToRoom = this.subscribeToRoom.bind(this)
+    this.getRooms = this.getRooms.bind(this)
+    this.createRoom = this.createRoom.bind(this)
+  }
+
+  componentDidMount() {
+    const chatManager = new Chatkit.ChatManager({
+      instanceLocator,
+      userId: 'rockd',
+      tokenProvider: new Chatkit.TokenProvider({
+        url: tokenUrl
+      })
+    })
+
+    chatManager.connect()
+      .then(currentUser => {
+        this.currentUser = currentUser
+        this.getRooms()
+      })
+      .catch(err => console.log('error on connecting', err))
+  }
+
+
+  getRooms = () => {
+    this.currentUser.getJoinableRooms()
+      .then(joinableRooms => {
+        this.setState({
+          joinableRooms,
+          joinedRooms: this.currentUser.rooms
+        })
+      })
+      .catch(err => console.log('error on joinableRooms', err))
+  }
+
+
+  subscribeToRoom = (roomId) => {
+    this.setState({ messages: [] }) 
+    this.currentUser.subscribeToRoom({
+      roomId: roomId,
+      hooks: {
+        onMessage: message => {
+          this.setState({
+            messages: [...this.state.messages, message] 
+          })
+        }
+      }
+    })
+      .then(room => {
+        this.setState({
+          roomId: room.id 
+        })
+        this.getRooms()  
+      })
+      .catch(err => console.log('Error on subscribing room', err))
+  }
+
+
+  sendMessage = (text) => {
+    this.currentUser.sendMessage({
+      text,
+      roomId: this.state.roomId
+    })
+
+  }
+
+  createRoom = (name) => {
+    //console.log('Room Name: ', name)
+    this.currentUser.createRoom({
+      name
+    })
+      .then(room => this.subscribeToRoom(room.id))
+      .catch(err => console.log('Error in creating Room ', err))
+  }
+
+  render() {
+    const { joinableRooms, joinedRooms } = this.state
+    // console.log('message :', this.state.messages);
+    return (
+      <div className="app">
+        <RoomList
+          roomId={this.state.roomId}
+          subscribeToRoom={this.subscribeToRoom}
+          rooms={[...joinableRooms, ...joinedRooms]}
+        />
+        <MessageList messages={this.state.messages} roomId={this.state.roomId} />
+        <SendMessageForm disabled={!this.state.roomId} sendMessage={this.sendMessage} />
+        <NewRoomForm createRoom={this.createRoom} />
+      </div>
+    );
+  }
 }
 
-export default App;
+export default App
---
+++ client/src/config.js
@@ -0,0 +1,5 @@
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/YOUR_TOKEN/token";
+const instanceLocator = "YOUR_KEY";
+
+exports.tokenUrl = tokenUrl;
+exports.instanceLocator = instanceLocator;
